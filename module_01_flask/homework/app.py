81bc06a702a7666f943482451211628aa1bc1601
First_user <sairus.89@mail.ru>
2022-04-19 23:48:13 +0300

    'review'

--- module_01_flask/homework/app.py
+++ module_01_flask/homework/app.py
@@ -1,34 +1,52 @@
 import datetime
+import random
 from flask import Flask
 
 app = Flask(__name__)
+global vist_counter
+vist_counter = 0
 
 
 @app.route('/hello_world')
-def test_function():
-   pass
+def hello_world():
+   return 'Hello, world!'
 
 
 @app.route('/cars')
-def test_function():
-   pass
+def cars():
+   return 'Chevrolet, Renault, Ford, Lada'
 
 
 @app.route('/cats')
-def test_function():
-   pass
+def any_cat():
+   cats = ['Корниш рекс', 'Русская голубая', 'Шотландская вислоухая', 'Мейн-кун', 'Манчкин']
+   return random.choice(cats)
 
 
 @app.route('/get_time/now')
-def test_function():
-   pass
+def get_time_now():
+   current_time = datetime.datetime.now()
+   return f'Точное время {current_time}'
 
 
 @app.route('/get_time/future')
-def test_function():
-   pass
+def get_time_future():
+   current_time = datetime.datetime.now()
+   future_time = datetime.timedelta(hours=1)
+   return f'Точное время через час будет {current_time + future_time}'
 
 
 @app.route('/get_random_word')
-def test_function():
-   pass
+def random_word():
+   try:
+      any_word = random.choice(random.choice(list(open('war_and_peace.txt', encoding='utf-8'))).split())
+   except IndexError:
+      any_word = 'any_word'
+   return f'{any_word}'
+
+
+@app.route('/visit')
+def visit_func():
+   global vist_counter
+   vist_counter += 1
+   return f'Page visit {vist_counter}'
--- module_01_flask/materials/first_application/app.py
+++ module_01_flask/materials/first_application/app.py
@@ -2,8 +2,23 @@ import datetime
 from flask import Flask
 
 app = Flask(__name__)
+global vist_counter
+vist_counter = 0
+
 
 @app.route('/test')
 def test_function():
    return 'Это тестовая страничка, ответ сгенерирован в %s' % \
                      datetime.datetime.now().utcnow()
+
+
+@app.route('/hello_world')
+def hello_world():
+   return 'Hello, world!'
+
+
+@app.route('/visit')
+def visit_func():
+   global vist_counter
+   vist_counter += 1
+   return f'Page visit {vist_counter}'
