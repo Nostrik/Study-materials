21cb8de7b1f3a8c8e536242d7e799f08da78e63d
Nostrik <sairus.89@mail.ru>
2022-05-15 22:28:58 +0300

    to do block_2_1

--- module_05_processes_and_threads/homework/block_2_1.py
+++ module_05_processes_and_threads/homework/block_2_1.py
@@ -1,12 +1,49 @@
+import signal
 import subprocess
 import time
+import os
+import re
+from pprint import pprint
+
+
+def get_pid_from_lsof():
+    proc = subprocess.Popen([f'lsof -i :5000'], shell=True, stdout=subprocess.PIPE)
+    data = (proc.communicate())[0].decode('utf-8')
+    # print(data)
+    pid_temp = data.split('\n')[1]
+    # print(pid_temp)
+    pid = pid_temp.split(' ')[1]
+    # print(pid)
+    pass
+    return pid
 
 
 def run_flask_server():
-    proc = subprocess.Popen(['python', '../materials/prev_hw_review/hw_3_1.py'])
-    print(proc.pid)
-    time.sleep(5)
-    proc_2 = subprocess.run(['kill ', f'%{proc.pid}'], shell=True)
+    print('* Running flask server')
+    time.sleep(1)
+    process = subprocess.run(['python3', '../materials/prev_hw_review/hw_3_1.py'],
+
+                             )
+    print(process.stdout)
+    if process.returncode == 0:
+        return True
+    else:
+        return False
+
+
+def check_running_process_on_port():
+    proc = run_flask_server()
+    print(proc)
+    # if proc.returncode is None:
+    #     print('* Running port is busy')
+    #     time.sleep(2)
+    #     pid_lsof = get_pid_from_lsof()
+    #     print('* Cleaning port')
+    #     os.kill(int(pid_lsof), signal.SIGTERM)
+    #     time.sleep(1)
+    #     check_running_process_on_port()
+    # else:
+    #     print(proc.stdout)
 
 
 if __name__ == '__main__':
--- module_05_processes_and_threads/materials/linux_process/simple_app.py
+++ module_05_processes_and_threads/materials/linux_process/simple_app.py
@@ -6,10 +6,10 @@ from flask import current_app
 app = flask.Flask(__name__)
 
 
-@app.endpoint('test')
+@app.route('/test')
 def test_endpoint():
     return 'Test endpoint was called!'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
