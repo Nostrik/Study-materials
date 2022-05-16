a23a7e5883ec6dadb785aafc64bdba86991eaad6
UbuUser <sairus.89@mail.ru>
2022-05-16 15:48:37 +0300

    to do block_2_2

--- module_05_processes_and_threads/homework/block_2_2.py
+++ module_05_processes_and_threads/homework/block_2_2.py
@@ -2,21 +2,22 @@ class MyManager:
     def __init__(self, path: str, mode, ex_list):
         self.name = path
         self.mode = mode
-        self.exeptions = ex_list
-        # print(self.exeptions)
+        self.exceptions = ex_list
 
     def __enter__(self):
         self.file = open(self.name, self.mode)
         return self.file
 
     def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type in self.exceptions:
+            return True
         print("Exception {} has been handled".format(exc_type))
         self.file.close()
-        return True
 
 
 if __name__ == '__main__':
-    ignore_exceptions = FileNotFoundError
+    ignore_exceptions = (ValueError, AttributeError)
     with MyManager('test_input.txt', 'r', ignore_exceptions) as file:
         s = file.read()
         print(s)
+        raise ValueError
