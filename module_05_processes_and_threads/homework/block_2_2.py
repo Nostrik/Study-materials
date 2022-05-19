39b7fe1eda41f2bc07801e7bc5ffe5ae0f5bef16
FalevMV <sairus.89@mail.ru>
2022-05-19 08:22:15 +0300

    hw module 5 done

--- module_05_processes_and_threads/homework/block_2_2.py
+++ module_05_processes_and_threads/homework/block_2_2.py
@@ -1,3 +1,6 @@
+import unittest
+
+
 class MyManager:
     def __init__(self, path: str, mode, ex_list):
         self.name = path
@@ -14,6 +17,38 @@ class MyManager:
             return True
         print("Exception {} has been handled".format(exc_type))
         self.file.close()
+
+
+class ErrorManager:
+    def __init__(self, errors: tuple):
+        self.errors_list = errors
+
+    def __enter__(self):
+        print('Starting to calculate')
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        if exc_type and exc_type.__name__ in self.errors_list:
+            print('Exception {} has been handled'.format(exc_type.__name__))
+            return True
+
+
+class TestErrorManager(unittest.TestCase):
+
+    def test_error_manager(self):
+        with ErrorManager(('IndexError', 'TypeError')):
+            a = 1 + 2
+        self.assertTrue(a == 3)
+
+        with self.assertRaises(ZeroDivisionError):
+            with ErrorManager(('IndexError', 'TypeError')):
+                10 * (1 / 0)
+
+        b = None
+        with ErrorManager(('IndexError', 'TypeError')):
+            b = 'sdf' + 2 + 5
+        with ErrorManager(('IndexError', 'TypeError')):
+            b = [1, 2][2]
+        self.assertIsNone(b)
 
 
 if __name__ == '__main__':
--- module_05_processes_and_threads/homework/block_3_1.py
+++ module_05_processes_and_threads/homework/block_3_1.py
@@ -1,25 +1,55 @@
 import sys
+import unittest
+from typing import TextIO
 
 
-class MyManager:
-    def __init__(self, new_stdout, new_stderr):
-        self.stdout = new_stdout
-        self.stderr = new_stderr
+class OutputManager:
+    def __init__(self, io_obj1: TextIO, io_obj2):
+        self.io_obj1 = io_obj1
+        self.io_obj2 = io_obj2
 
     def __enter__(self):
-        sys.stdout = self.stdout
-        sys.stderr = self.stderr
+        #  При входе в контекст нужно сохранять текущие
+        #  значения sys.stdout и sys.stderr, а при выходе
+        #  возвращать их в исxодное состояние.
+        self.current_stdout, self.current_stderr = sys.stdout, sys.stderr
+        sys.stdout, sys.stderr = self.io_obj1, self.io_obj2
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.stdout.close()
-        self.stderr.close()
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        print('Exception {} has been handled'.format(exc_type))
+        sys.stdout, sys.stderr = self.current_stdout, self.current_stderr
+        # Поток io_obj можно здесь не закрывать.
+        return True
+
+
+class TestOutputManager(unittest.TestCase):
+    def setUp(self) -> None:
+        file1 = open('file1.txt', 'w')
+        file2 = open('file2.txt', 'w')
+        with OutputManager(file1, file2):
+            print('Print to stdout')
+            print('Print to stderr', file=sys.stderr)
+            10 * 1 / 0
+        file1.close()
+        file2.close()
+
+    def test_output_manager(self):
+        with open('file1.txt', 'r') as file:
+            text = file.read()
+        self.assertIn('Print to stdout', text)
+        self.assertIn('ZeroDivisionError', text)
+
+        with open('file2.txt', 'r') as file:
+            text = file.read()
+        self.assertIn('Print to stderr', text)
 
 
 if __name__ == '__main__':
-    with MyManager(
-            new_stdout=open('stdout.txt', 'a', encoding='utf-8'),
-            new_stderr=open('stderr.txt', 'a', encoding='utf-8')
-    ):
-        print('ТЕКСТ stdout')
-        print(12/0)
-        raise ZeroDivisionError
+    file1 = open('file1.txt', 'w')
+    file2 = open('file2.txt', 'w')
+    with OutputManager(file1, file2):
+        print('Print to stdout')
+        print('Print to stderr', file=sys.stderr)
+        10 * 1 / 0
+    file1.close()
+    file2.close()
