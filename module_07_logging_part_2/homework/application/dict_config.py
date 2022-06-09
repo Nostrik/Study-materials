9517d7de02f6ca6c79c780ff865070b25ec7d49a
FalevMV <sairus.89@mail.ru>
2022-06-09 11:51:09 +0300

    to do hw mod 7

--- module_07_logging_part_2/homework/application/dict_config.py
+++ module_07_logging_part_2/homework/application/dict_config.py
@@ -3,15 +3,20 @@ import logging
 
 class CustomFileHandlerForHwMod07(logging.Handler):
 
-    def __init__(self, file_name, mode='w'):
+    def __init__(self, file_1, file_2,  mode='w'):
         super().__init__()
-        self.file = file_name
+        self.file_1 = file_1
+        self.file_2 = file_2
         self.mode = mode
 
     def emit(self, record: logging.LogRecord) -> None:
         msg = self.format(record)
-        with open(self.file, mode=self.mode) as f:
-            f.write(msg + '\n')
+        if record.levelname == "DEBUG":
+            with open(self.file_1, mode=self.mode) as f:
+                f.write(msg + '\n')
+        elif record.levelname == "INFO":
+            with open(self.file_2, mode=self.mode) as f:
+                f.write(msg + '\n')
 
 
 dict_config = {
@@ -27,13 +32,26 @@ dict_config = {
             "class": "logging.StreamHandler",
             "level": "DEBUG",
             "formatter": "base"
+        },
+        "file": {
+            "()": CustomFileHandlerForHwMod07,
+            "level": "DEBUG",
+            "formatter": "base",
+            "file_1": "debug_file.log",
+            "file_2": "info_file.log",
+            "mode": "a"
         }
     },
     "loggers": {
         "module_calc": {
             "level": "DEBUG",
-            "handlers": ["console"],
+            "handlers": ["console", "file"],
             # "propagate": False
+        },
+        "module_utils": {
+            "level": "DEBUG",
+            "handlers": ["console", "file"],
+            # "propagate": True
         }
     },
     "filters": {},
--- module_07_logging_part_2/homework/application/utils.py
+++ module_07_logging_part_2/homework/application/utils.py
@@ -1,6 +1,8 @@
 import logging
+import logging.config
 from typing import Union, Callable
 from operator import sub, mul, truediv, add
+from dict_config import dict_config
 
 
 OPERATORS = {
@@ -11,13 +13,13 @@ OPERATORS = {
 }
 
 Numeric = Union[int, float]
-
+logging.config.dictConfig(dict_config)
 logger = logging.getLogger('module_utils')
-formatter = logging.Formatter(fmt="%(levelname)s | %(name)s | %(asctime)s | %(lineno)d | %(message)s |")
-logger.setLevel("DEBUG")
-handler = logging.StreamHandler()
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+# formatter = logging.Formatter(fmt="%(levelname)s | %(name)s | %(asctime)s | %(lineno)d | %(message)s |")
+# logger.setLevel("DEBUG")
+# handler = logging.StreamHandler()
+# handler.setFormatter(formatter)
+# logger.addHandler(handler)
 logger.debug('import utils')
 
 
