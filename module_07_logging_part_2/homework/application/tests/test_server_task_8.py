7a12018b5ac4ce90ac562d9b6936c6608f88b57b
UbuUser <sairus.89@mail.ru>
2022-06-15 15:51:12 +0300

    last task

--- module_07_logging_part_2/homework/application/tests/test_server_task_8.py
+++ module_07_logging_part_2/homework/application/tests/test_server_task_8.py
@@ -10,13 +10,39 @@ class TestLogHandler(unittest.TestCase):
         self.app_server = app.test_client()
         self.base_url = '/log-entry'
 
-    def test_server_if_send_simple_msg(self):
-        simple_msg = 'simple message'
-        response = self.app_server.post(self.base_url, data={"msg": simple_msg})
-        code = response.status_code
-        response_text = response.data.decode()
-        self.assertTrue(simple_msg in response_text)
+    # def test_server_if_send_simple_msg(self):
+    #     simple_msg = 'simple message'
+    #     response = self.app_server.post(self.base_url, data={"msg": simple_msg})
+    #     code = response.status_code
+    #     response_text = response.data.decode()
+    #     self.assertTrue(simple_msg in response_text)
 
-    def test_handler(self):
+    def test_handler_debug_level(self):
         run_handler = main(0)
-        pass
+        with open('task_8_log_ser.log', 'r') as log_file:
+            text = log_file.read()
+        # self.assertTrue('DEBUG' in text)
+
+    # def test_handler_info_level(self):
+    #     run_handler = main(1)
+    #     with open('task_8_log_ser.log', 'r') as log_file:
+    #         text = log_file.read()
+    #     self.assertTrue('INFO' in text)
+    #
+    # def test_handler_error_level(self):
+    #     run_handler = main(2)
+    #     with open('task_8_log_ser.log', 'r') as log_file:
+    #         text = log_file.read()
+    #     self.assertTrue('ERROR' in text)
+    #
+    # def test_handler_critical_level(self):
+    #     run_handler = main(3)
+    #     with open('task_8_log_ser.log', 'r') as log_file:
+    #         text = log_file.read()
+    #     self.assertTrue('CRITICAL' in text)
+    #
+    # def test_handler_warning_level(self):
+    #     run_handler = main(4)
+    #     with open('task_8_log_ser.log', 'r') as log_file:
+    #         text = log_file.read()
+    #     self.assertTrue('WARNING' in text)
--- module_07_logging_part_2/homework/task_8_server.py
+++ module_07_logging_part_2/homework/task_8_server.py
@@ -1,12 +1,21 @@
+import logging
 from flask import Flask, request
 
 
 app = Flask(__name__)
+logger = logging.getLogger('server_logger')
+logging.basicConfig()
+logger.setLevel('DEBUG')
+handler = logging.FileHandler('task_8_log_ser.log', mode='w')
+formatter = logging.Formatter(fmt="(levelname)s | %(name)s | %(asctime)s | %(lineno)d | %(message)s |")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 
 @app.route("/log-entry", methods=["POST"])
 def accept_log_entry():
     msg = request.form.get('msg')
+    logger.debug(msg)
     return f'{msg}'
 
 
@@ -17,4 +26,5 @@ def query_example():
 
 if __name__ == '__main__':
     app.run(debug=True)
-#  https://www.digitalocean.com/community/tutorials/processing-incoming-request-data-in-flask-ru
+    # logging.basicConfig(level=logging.debug(), filename='task_8_log_ser.log')
+    #  curl -X POST http://localhost:5000/log-entry --data "msg=test"
