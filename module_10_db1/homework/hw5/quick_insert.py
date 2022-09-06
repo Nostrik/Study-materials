25dbe6e81a17c9a7294e69fd757e61e07dff90b9
FalevMV <sairus.89@mail.ru>
2022-09-06 13:28:35 +0300

    mod 11 pr

--- module_10_db1/homework/hw5/quick_insert.py
+++ module_10_db1/homework/hw5/quick_insert.py
@@ -1,22 +1,45 @@
 import functools
 from typing import Union
+import time
+import numpy
 
 Number = Union[int, float, complex]
 
 
-def find_insert_position(array: list[Number], number: Number) -> int:
-    position = 0
-    if len(array) < 1:
-        raise ValueError
-    while True:
-        array.insert(position, number)
-        sort_list = array.copy()
-        sort_list.sort()
-        if functools.reduce(lambda x, y: x and y, map(lambda p, q: p == q, array, sort_list)):
-            break
-        array.pop(position)
-        position += 1
-    return position
+# def find_insert_position(array: list[Number], number: Number) -> int:
+#     position = 0
+#     if len(array) < 1:
+#         raise ValueError
+#     while True:
+#         array.insert(position, number)
+#         sort_list = array.copy()
+#         sort_list.sort()
+#         if functools.reduce(lambda x, y: x and y, map(lambda p, q: p == q, array, sort_list)):
+#             break
+#         array.pop(position)
+#         position += 1
+#     return position
+
+def find_insert_position(struct, elem):
+    low = 0
+    high = len(struct)
+    while low < high:
+        mid = (low + high) // 2
+        if struct[mid] < elem:
+            low = mid + 1
+        else:
+            high = mid
+
+    return low
+
+
+A = numpy.random.random_integers(1, 10 ** 9, 10 ** 6)
+A.sort()
+
+current = time.time()
+print('result', find_insert_position(A, random.randint(1, 10 ** 8)))
+after = time.time()
+print('time', after - current)
 
 
 if __name__ == '__main__':
