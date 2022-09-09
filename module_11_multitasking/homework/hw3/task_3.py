3cc4cc5430e0814b3bd70fddd72ba72396d30830
FalevMV <sairus.89@mail.ru>
2022-09-09 13:24:06 +0300

    to do mod 11 hw

--- module_11_multitasking/homework/hw3/task_3.py
+++ module_11_multitasking/homework/hw3/task_3.py
@@ -13,20 +13,21 @@ characters = []
 lock_db = Lock()
 
 
-def get_character(number: int):
+def get_character(number: int, plug: str):
     response = requests.get(f'https://swapi.dev/api/people/{number}/')
     logger.info(f'Request number {number} to swapi..')
     if response.status_code != 200:
         logger.error('SWAPI is not available!')
     else:
         character = response.json()
+        lock_db.acquire()
         try:
             characters.append({
                 'name': character['name'],
                 'birth_year': character['birth_year'],
                 'gender': character['gender']
             })
-            with sqlite3.connect("task_2_swapi.db") as conn:
+            with sqlite3.connect("task_3_swapi.db") as conn:
                 cursor = conn.cursor()
                 cursor.execute('INSERT INTO characters (name, birth_year, gender) VALUES ("{}", "{}", "{}")'.format(
                     character["name"], character["birth_year"], character["gender"]
@@ -34,12 +35,16 @@ def get_character(number: int):
             logger.info(f'Characters name {character["name"]} is added')
         except Exception as er:
             logger.exception(er)
+        lock_db.release()
 
 
 def main():
     start_time = time.time()
     for i in range(1, 21):
-        get_character(i)
+        thread = threading.Thread(
+            target=get_character, args=(i, 'plug')
+        )
+        thread.start()
     time.sleep(3)
     logger.info(f'Time spent: {time.time() - start_time} - 3s')
     pprint(characters)
