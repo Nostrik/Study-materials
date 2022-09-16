8da21d61c725cd5ff6b61452312356ab274434b4
Maksim <sairus.89@mail.ru>
2022-09-16 15:41:35 +0300

    to do task 2 mod 21

--- module_12_multitasking_2/homework/hw2/task.py
+++ module_12_multitasking_2/homework/hw2/task.py
@@ -1,8 +1,26 @@
+import threading
+import subprocess
+from pprint import pprint
+
+
 def process_count(username: str) -> int:
     # к-во процессов, запущенных из-под текущего пользователя
+    proc = subprocess.Popen(
+        ["ps", "U", "maksim"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+    res = proc.communicate()
+    for line in res:
+        print(line.decode("utf-8"))
     pass
 
 
 def total_memory_usage(root_pid: int) -> int:
     # суммарное потребление памяти древа процессов
     pass
+
+
+if __name__ == "__main__":
+    process_count("test_name")
+    # print("I'm here")
