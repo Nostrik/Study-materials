c30b5a84e529ff5291ca76fb79a6642462cb8c4a
Maksim <sairus.89@mail.ru>
2022-09-19 16:38:41 +0300

    todo task_3

--- module_12_multitasking_2/homework/hw2/task.py
+++ module_12_multitasking_2/homework/hw2/task.py
@@ -1,26 +1,21 @@
 import threading
+import psutil
 import subprocess
 from pprint import pprint
 
 
 def process_count(username: str) -> int:
-    # к-во процессов, запущенных из-под текущего пользователя
-    proc = subprocess.Popen(
-        ["ps", "U", "maksim"],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE
-    )
-    res = proc.communicate()
-    for line in res:
-        print(line.decode("utf-8"))
-    pass
+    proc_dict = {proc.pid: proc.name() for proc in psutil.process_iter() if proc.username() == username}
+    return int(len(proc_dict))
 
 
 def total_memory_usage(root_pid: int) -> int:
     # суммарное потребление памяти древа процессов
-    pass
+    process = psutil.Process(root_pid)
+    return int(process.memory_info().vms)
 
 
 if __name__ == "__main__":
-    process_count("test_name")
+    # print(process_count("maksim"))
     # print("I'm here")
+    # total_memory_usage(1555)
