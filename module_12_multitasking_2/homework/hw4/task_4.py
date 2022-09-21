b793b1550c0a48be07379ae4bb4069c50b52ce62
Maksim <sairus.89@mail.ru>
2022-09-21 15:13:30 +0300

    todo task_4 mod12

--- module_12_multitasking_2/homework/hw4/task_4.py
+++ module_12_multitasking_2/homework/hw4/task_4.py
@@ -1,12 +1,26 @@
 import logging
 import requests
+import time
 
 
 logging.basicConfig(
-    filename="task_4.log",
+
     level=logging.INFO,
-    format="<%(threadName)s> - <%(asctime)s>"
+    format="<%(threadName)s> - <%(message)s>"
 )
 logger = logging.getLogger(__name__)
+date_url = "https://showcase.api.linx.twenty57.net/UnixTime/fromunix"
 
 
+def get_time_from_api_linux() -> str:
+    """
+    The function returns the current time using unix time stamp, converting it to human readable format.
+    """
+    from_time = str(time.time())[:10]
+    request = requests.get(date_url, params={'timestamp': from_time})
+    return request.text
+
+
+if __name__ == "__main__":
+    print(get_time_from_api_linux())
+    logger.info(get_time_from_api_linux())
