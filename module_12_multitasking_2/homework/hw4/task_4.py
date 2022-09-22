fdb94f3aedf0beb6308b90a21fe4edbb72fdbcb8
Maksim <sairus.89@mail.ru>
2022-09-22 15:51:50 +0300

    mod12 task4 question

--- module_12_multitasking_2/homework/hw4/task_4.py
+++ module_12_multitasking_2/homework/hw4/task_4.py
@@ -1,6 +1,10 @@
 import logging
+import multiprocessing
+
 import requests
 import time
+from multiprocessing.pool import ThreadPool
+from multiprocessing import Pool
 
 
 logging.basicConfig(
@@ -10,17 +14,25 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 date_url = "https://showcase.api.linx.twenty57.net/UnixTime/fromunix"
+list_urls = [date_url] * 10
 
 
-def get_time_from_api_linux() -> str:
+def work_func(url):
     """
     The function returns the current time using unix time stamp, converting it to human readable format.
+    Writes a message log within 20 seconds.
     """
     from_time = str(time.time())[:10]
-    request = requests.get(date_url, params={'timestamp': from_time})
-    return request.text
+    request = requests.get(url, params={'timestamp': from_time})
+    time_start = time.time()
+    current_time = 0
+    while current_time - time_start < 10:
+        logger.info(request.text)
+        current_time = time.time()
 
 
 if __name__ == "__main__":
-    print(get_time_from_api_linux())
-    logger.info(get_time_from_api_linux())
+    pool = ThreadPool(processes=10)
+    result = pool.map(work_func, list_urls)
+    pool.close()
+    pool.join()
