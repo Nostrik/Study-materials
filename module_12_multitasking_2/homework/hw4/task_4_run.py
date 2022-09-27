5e80fe3bc7011db61a97a0bbd5976708eab6d383
Maksim <sairus.89@mail.ru>
2022-09-27 10:53:57 +0300

    todo task4

--- module_12_multitasking_2/homework/hw4/task_4_run.py
+++ module_12_multitasking_2/homework/hw4/task_4_run.py
@@ -1,9 +1,20 @@
 import subprocess
-import requests
+from pprint import pprint
+
+
+def sort_temp_file():
+    file = open("temp_log.txt", 'r')
+    for line in file:
+        tmp_list.append(line.rstrip())
+    # sorted(tmp_list, key=lambda d: d[14:33])
 
 
 if __name__ == "__main__":
+    tmp_list = []
     print('task_4 start..')
     proc = subprocess.Popen(['python', 'task_4.py'])
     proc.wait()
     print("You can start sort temp file")
+    sort_temp_file()
+    pprint(tmp_list)
+    # pprint(sorted(tmp_list, key=lambda d: d[14:33]))
