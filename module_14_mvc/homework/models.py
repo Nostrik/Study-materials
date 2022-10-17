04a9d260f43451b548cca2ae9ec93a0582c0acb2
First_user <sairus.89@mail.ru>
2022-10-17 18:57:24 +0300

    last task

--- module_14_mvc/homework/models.py
+++ module_14_mvc/homework/models.py
@@ -1,22 +1,27 @@
 import sqlite3
-from typing import Any, Optional
+import logging
+from typing import Any, Optional, List
 
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger('[from models]')
 
 DATA: list[dict] = [
-    {'id': 0, 'title': 'A Byte of Python', 'author': 'Swaroop C. H.'},
-    {'id': 1, 'title': 'Moby-Dick; or, The Whale', 'author': 'Herman Melville'},
-    {'id': 3, 'title': 'War and Peace', 'author': 'Leo Tolstoy'},
+    {'id': 0, 'title': 'A Byte of Python', 'author': 'Swaroop C. H.', 'views_cnt': 0},
+    {'id': 1, 'title': 'Moby-Dick; or, The Whale', 'author': 'Herman Melville', 'views_cnt': 0},
+    {'id': 3, 'title': 'War and Peace', 'author': 'Leo Tolstoy', 'views_cnt': 0},
 ]
 
 
 class Book:
 
-    def __init__(self, id: int, title: str, author: str) -> None:
+    def __init__(self, id: int, title: str, author: str, views_cnt: int) -> None:
         self.id: int = id
         self.title: str = title
         self.author: str = author
+        self.views_cnt: int = views_cnt
 
-    def __getitem__(self, item: str) -> Any:
+    def __getitem__(self, item) -> Any:
         return getattr(self, item)
 
 
@@ -37,17 +42,18 @@ def init_db(initial_records: list[dict]) -> None:
                 CREATE TABLE `table_books` (
                     id INTEGER PRIMARY KEY AUTOINCREMENT, 
                     title, 
-                    author 
+                    author,
+                    views_cnt
                 )
                 """
             )
             cursor.executemany(
                 """
                 INSERT INTO `table_books`
-                (title, author) VALUES (?, ?)
+                (title, author, views_cnt) VALUES (?, ?, ?)
                 """,
                 [
-                    (item['title'], item['author'])
+                    (item['title'], item['author'], item['views_cnt'])
                     for item in initial_records
                 ]
             )
@@ -61,7 +67,9 @@ def get_all_books() -> list[Book]:
             SELECT * from `table_books`
             """
         )
-        return [Book(*row) for row in cursor.fetchall()]
+        # result = [Book(*row) for row in cursor.fetchall()]
+        result = [increment_views_count(Book(*row)) for row in cursor.fetchall()]
+        return result
 
 
 def add_new_book(book_title, author_name):
@@ -73,3 +81,52 @@ def add_new_book(book_title, author_name):
             (title, author) VALUES (?, ?);
             """, (book_title, author_name)
         )
+
+
+def get_book_by_author(name) -> List[Book]:
+    with sqlite3.connect('table_books.db') as conn:
+        cursor: sqlite3.Cursor = conn.cursor()
+        cursor.execute(
+            """
+            SELECT * from `table_books`
+            WHERE author = ?
+            """, (name, )
+        )
+        res = [Book(*row) for row in cursor.fetchall()]
+        return res
+
+
+def get_book_by_id(book_id):
+    with sqlite3.connect('table_books.db') as conn:
+        cursor: sqlite3.Cursor = conn.cursor()
+        cursor.execute(
+            """
+            SELECT * FROM `table_books`
+            WHERE id = ?
+            """, (book_id, )
+        )
+        return [Book(*row) for row in cursor.fetchall()]
+
+
+def increment_views_count(book_obj):
+    with sqlite3.connect('table_books.db') as conn:
+        cursor: sqlite3.Cursor = conn.cursor()
+        cursor.execute(
+            """
+            SELECT views_cnt FROM `table_books`
+            WHERE id = ?
+            """, (book_obj.id, )
+        )
+        value = cursor.fetchone()
+        logger.debug(f'value is - {value[0]}')
+        int_value = int(value[0])
+        int_value += 1
+        cursor.execute(
+            """
+            UPDATE `table_books`
+            SET views_cnt = ?
+            WHERE id = ?
+            """, (book_obj.id, str(int_value))
+        )
+    return book_obj
+
--- module_14_mvc/homework/routes.py
+++ module_14_mvc/homework/routes.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from flask import request
-from models import init_db, get_all_books, DATA, add_new_book
+from models import init_db, get_all_books, DATA, add_new_book, get_book_by_author, get_book_by_id
 import logging
 from flask_wtf import FlaskForm
 from wtforms import StringField
@@ -12,8 +12,8 @@ logger = logging.getLogger(__name__)
 
 
 class AddBookForm(FlaskForm):
-    book_title = StringField(validators=[InputRequired])
-    book_author = StringField(validators=[InputRequired])
+    book_title = StringField(validators=[InputRequired()])
+    author_name = StringField(validators=[InputRequired()])
 
 
 def _get_html_table_for_books(books: list[dict]) -> str:
@@ -41,6 +41,7 @@ def _get_html_table_for_books(books: list[dict]) -> str:
 
 @app.route('/books')
 def all_books() -> str:
+    # logger.debug(f'------>  {get_all_books()}')
     return render_template(
         'index.html',
         books=get_all_books(),
@@ -51,20 +52,33 @@ def all_books() -> str:
 def get_books_form():
     add_form = AddBookForm()
     if request.method == 'POST':
-        # title = request.form.get('book_title')
-        # author = request.form.get('author_name')
-        # title = request.form['book_title']
-        # author = request.form['author_name']
-        title, author = add_form.book_title, add_form.book_author
-        add_new_book(title, author)
-        logger.debug(f'title is - {title}')
-        logger.debug(f'author is - {author}')
-        return f'Book added successful'
+        if add_form.validate_on_submit():
+            # title = request.form.get('book_title')
+            # author = request.form.get('author_name')
+            # title = request.form['book_title']
+            # author = request.form['author_name']
+            title, author = add_form.book_title.data, add_form.author_name.data
+            logger.debug(f'title is - {title}')
+            logger.debug(f'author is - {author}')
+            add_new_book(title, author)
+            return f'Book added successful'
+        return f"Invalid input, {add_form.errors}", 400
     return render_template('add_book.html')
-    # return f"Invalid input, {add_form.errors}", 400
-    # return render_template('add_book.html')
+
+
+@app.route('/books/<author_name>')
+def get_book_by(author_name):
+    logger.debug('TEST MSG')
+    return render_template('show_books_by.html', books=get_book_by_author(author_name))
+
+
+@app.route('/books/<id>')
+def book_id(id_book):
+    logger.debug(f'book_id is - {id_book}')
+    return render_template()
 
 
 if __name__ == '__main__':
     init_db(DATA)
+    app.config["WTF_CSRF_ENABLED"] = False
     app.run(debug=True)
