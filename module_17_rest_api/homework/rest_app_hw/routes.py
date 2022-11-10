efc5de1514e6264559c352969a57d9b8a69d7577
maksim <sairus.89@mail.ru>
2022-11-10 15:30:34 +0300

    pr mod 17

--- module_17_rest_api/homework/rest_app_hw/routes.py
+++ module_17_rest_api/homework/rest_app_hw/routes.py
@@ -1,17 +1,77 @@
-from flask import Flask
-from flask_restful import Resource, Api
+import logging
+from typing import Tuple, List, Dict
+
+from flask import Flask, request
+from flask_restful import Api, Resource
+from marshmallow import ValidationError
+
+from models import (
+    DATA,
+    get_all_books,
+    init_db,
+    add_book,
+    get_book_by_id,
+    update_book_by_id,
+    delete_book_by_id,
+)
+from schemas import BookSchema
 
 app = Flask(__name__)
 api = Api(app)
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger('[routes]')
 
 
-class HelloWorld(Resource):
-	def get(self):
-		return {'hello': 'world'}
+class BookList(Resource):
+    def get(self) -> Tuple[List[Dict], int]:
+        schema = BookSchema()
+        return schema.dump(get_all_books(), many=True), 200
+
+    def post(self) -> Tuple[Dict, int]:
+
+        data = request.json
+
+        schema = BookSchema()
+
+        try:
+            book = schema.load(data)
+        except ValidationError as exc:
+            return exc.messages, 400
+
+        book = add_book(book)
+        return schema.dump(book), 201
 
 
-api.add_resource(HelloWorld, '/')
+class Book(Resource):
+    def get(self, b_id) -> Tuple[List[Dict], int]:
+        logger.debug(b_id)
+        schema = BookSchema()
+        return schema.dump(get_book_by_id(b_id), many=True), 200
+
+    def put(self, b_id) -> Tuple[Dict, int]:
+
+        data = request.json
+        logger.debug(data)
+
+        schema = BookSchema()
+
+        try:
+            book = schema.load(data)
+        except ValidationError as exc:
+            return exc.messages, 400
+
+        book = update_book_by_id(book)
+        return schema.dump(book), 201
+
+    def delete(self, b_id):
+        delete_book_by_id(b_id)
+        return '', 204
+
+
+api.add_resource(BookList, '/api/books')
+api.add_resource(Book, '/api/books/<id>')
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    init_db(initial_records=DATA)
+    app.run(debug=True)
---
+++ module_17_rest_api/homework/rest_app_hw/schemas.py
@@ -0,0 +1,32 @@
+from typing import Dict
+
+from marshmallow import Schema, fields, validates, ValidationError, post_load
+
+from models import get_book_by_title, Book, get_book_by_author
+
+
+class BookSchema(Schema):
+
+    id = fields.Int(dump_only=True)
+    title = fields.Str(required=True)
+    author = fields.Str(required=True)
+
+    @validates('title')
+    def validate_title(self, title: str) -> None:
+        if get_book_by_title(title) is not None:
+            raise ValidationError(
+                'Book with title "{title}" already exists, '
+                'please use a different title.'.format(title=title)
+            )
+
+    @validates('author')
+    def validate_author(self, author: str) -> None:
+        if get_book_by_author(author) is not None:
+            raise ValidationError(
+                'Book with title "{author}" already exists, '
+                'please use a different title.'.format(author=author)
+            )
+
+    @post_load
+    def create_book(self, data: Dict, **kwargs) -> Book:
+        return Book(**data)
