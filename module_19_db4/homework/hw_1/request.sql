d171ee5de089ed212dc8a35e7bab185ac7bddefa
Maksim Falev <sairus.89@mail.ru>
2022-11-17 22:05:19 +0300

    task 3 mod 19

---
+++ module_19_db4/homework/hw_1/request.sql
@@ -0,0 +1,8 @@
+SELECT avg(grade),
+teachers.full_name
+FROM assignments_grades
+JOIN assignments
+ON assignments_grades.assisgnment_id = assignments.assisgnment_id
+JOIN teachers
+ON assignments.teacher_id = teachers.teacher_id
+GROUP BY assignments.teacher_id
---
+++ module_19_db4/homework/hw_3/request.sql
@@ -0,0 +1,12 @@
+SELECT avg(grade) as avg_grade,
+assignments_grades.assisgnment_id,
+assignments_grades.student_id,
+assignments.teacher_id,
+students.full_name
+FROM assignments_grades
+JOIN assignments
+ON assignments_grades.assisgnment_id = assignments.assisgnment_id
+JOIN students
+ON assignments_grades.student_id = students.student_id
+GROUP by assignments.teacher_id
+order by avg_grade desc
