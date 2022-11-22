cae0f8f3b60d04aa1613d9af0e61890bb76e1012
maksim <sairus.89@mail.ru>
2022-11-22 08:34:16 +0300

    review 3

--- module_19_db4/homework/hw_4/request.sql
+++ module_19_db4/homework/hw_4/request.sql
@@ -1,39 +1,10 @@
-SELECT avg(assignments_grades.grade),
-max(assignments_grades.grade),
-min(assignments_grades.grade)
-FROM assignments_grades
-JOIN assignments
-ON assignments_grades.assisgnment_id = assignments.assisgnment_id
-WHERE assignments_grades.date > assignments.due_date AND assignments.assisgnment_id = 1
-
-SELECT *
-FROM
-(SELECT assignments.group_id,
-assignments.due_date,
-asg.date
-FROM assignments
-INNER JOIN assignments_grades asg
-ON assignments.assisgnment_id = asg.assisgnment_id
-WHERE asg.date > assignments.due_date)
-ORDER by group_id
-
-SELECT count(*)
-FROM
-(SELECT assignments.due_date as DUE_DATE,
-assignments_grades.date as DATE,
-assignments.group_id as GROUP_ID
-FROM assignments
-INNER JOIN assignments_grades
-ON assignments.assisgnment_id = assignments_grades.assisgnment_id)
-WHERE DATE > DUE_DATE
-
-SELECT *
-FROM
-(SELECT assignments.due_date as DUE_DATE,
-assignments_grades.date as DATE,
-assignments.group_id as GROUP_ID
-FROM assignments
-INNER JOIN assignments_grades
-ON assignments.assisgnment_id = assignments_grades.assisgnment_id)
-WHERE DATE > DUE_DATE
+SELECT A.group_id,
+        avg(AG.grade) as Average,
+        max(AG.grade) as Maximum,
+        min(AG.grade) as Minimum
+FROM assignments_grades AG
+    JOIN assignments A
+    ON AG.assisgnment_id = A.assisgnment_id
+WHERE AG.date > A.due_date
+GROUP BY A.group_id
 
