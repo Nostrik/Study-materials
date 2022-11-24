568488f42d39aa1969c1ac7c46c8a08cd14ea295
maksim <sairus.89@mail.ru>
2022-11-24 13:05:16 +0300

    do hw mod 20

--- module_20_orm_1/homework/hw.py
+++ module_20_orm_1/homework/hw.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Table, create_engine, MetaData, Column, Integer, String, Float, UniqueConstraint, Index
+from sqlalchemy import Table, create_engine, MetaData, Column, Integer, String, Date, Float, Boolean, DateTime, UniqueConstraint, Index
 from sqlalchemy.orm import sessionmaker, mapper, declarative_base
 from sqlalchemy import create_engine
 from flask import Flask, jsonify, abort, request
@@ -6,7 +6,71 @@ from flask import Flask, jsonify, abort, request
 app = Flask(__name__)
 engine = create_engine('sqlite:///homework.db')
 Base = declarative_base()
-Seesion = sessionmaker(bind=engine)
-sesion = Seesion()
+Session = sessionmaker(bind=engine)
+session = Session()
 
 
+class Book(Base):
+    __tablename__ = 'books'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(20), nullable=False)
+    count = Column(Integer, default=1)
+    release_date = Column(Date, nullable=False)
+    author_id = Column(Integer, nullable=False)
+
+    def __repr__(self):
+        return F"The book {self.name}, in count: {self.count}, author_id: {self.author_id}"
+
+    def to_json(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+
+class Author(Base):
+    __tablename__ = 'authors'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(20), nullable=False)
+    surname = Column(String(20), nullable=False)
+
+    def __repr__(self):
+        return f"The Author is {self.name}, id: {self.id}"
+
+
+class Student(Base):
+    __tablename__ = 'students'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(20), nullable=False)
+    surname = Column(String(20), nullable=False)
+    phone = Column(String(10), nullable=False)
+    email = Column(String(10), nullable=False)
+    average_score = Column(Float, nullable=False)
+    scholarship = Column(Boolean, nullable=False)
+
+    def __repr__(self):
+        return f"The student is {self.name}, id: {self.id}"
+
+
+class ReceiveBook(Base):
+    __tablename__ = 'receiving_books'
+
+    id = Column(Integer, primary_key=True)
+    book_id = Column(Integer, nullable=False)
+    student_id = Column(Integer, nullable=False)
+    date_of_issue = Column(DateTime, nullable=False)
+    date_of_return = Column(DateTime)
+
+
+@app.before_request
+def before_request_func():
+    Base.metadata.create_all(engine)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello_world'
+
+
+if __name__ == "__main__":
+    app.run()
