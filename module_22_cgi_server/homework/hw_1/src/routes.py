f1f8488350776a372734b75b5c6aca79ffa90eab
Nostrik <sairus.89@mail.ru>
2022-12-19 22:03:40 +0300

    wsgi_app_work_return_json_problem

--- module_22_cgi_server/homework/hw_1/src/routes.py
+++ module_22_cgi_server/homework/hw_1/src/routes.py
@@ -1,21 +1,36 @@
 from flask import jsonify, Flask
+import logging
+import json
 
-application = Flask(__name__)
+# application = Flask(__name__)
+#
+#
+# @application.route('/hello')
+# @application.route('/hello/<username>')
+# def hello_world(username='username'):
+#     return jsonify(message='hello', name=username)
+
+logging.basicConfig(level=logging.INFO)
+r_logger = logging.getLogger("[r_logger]")
 
 
-@application.route('/hello')
-@application.route('/hello/<username>')
-def hello_world(username='username'):
-    return jsonify(message='hello', name=username)
-
-# def application(environ, start_response):
-#     status = '200 OK'
-#     output = 'Hello World!\n'
-#     response_headers = [('Content-type', 'text/plain'),
-#                         ('Content-Length', str(len(output)))]
-#     start_response(status, response_headers)
-#     if environ['url'] == '/hello':
-#         return jsonify(output)
-#     else:
-#         get_username = environ['url'].split('/')
-#         return jsonify(message='hello', username=get_username[1])
+def application(environ, start_response):
+    status = '200 OK'
+    output = 'Hello World!\n'
+    response_headers = [('Content-type', 'text/plain'),
+                        ('Content-Length', str(len(output)))]
+    start_response(status, response_headers)
+    # for k in environ:
+    #     r_logger.info('-' * 50)
+    #     r_logger.info(k)
+    #     r_logger.info(environ[k])
+    #     r_logger.info('-' * 50)
+    data = {'message': 'hello', 'name': 'username'}
+    if environ['REQUEST_URI'] == '/hello':
+        return jsonify(message='hello', name='username')
+        # return json.dumps(message='hello', name='username')
+        # return json.dump(data)
+    else:
+        get_username = environ['REQUEST_URI'].split('/')
+        r_logger.info(get_username)
+        return jsonify(message='hello', name=get_username[2])
