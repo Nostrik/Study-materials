0de2e6d56dbe29af2e286df856a0c6fabe85e576
First_user <sairus.89@mail.ru>
2023-01-31 21:43:27 +0300

    task3

--- module_25_asynchronous_programming/homework/hw_3/hw_3_internet_crawler.py
+++ module_25_asynchronous_programming/homework/hw_3/hw_3_internet_crawler.py
@@ -1,11 +1,26 @@
 from loguru import logger
+from bs4 import BeautifulSoup
 import aiohttp
 import asyncio
+import requests
 import time
+
+HEADERS = ''
+URL = 'https://www.geeksforgeeks.org/'
+reqs = requests.get(URL)
+soup = BeautifulSoup(reqs.text, 'html.parser')
+urls = []
+
+
+async def get_html(url: str):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, headers=HEADERS) as response:
+            return await response.text()
 
 
 def main():
-    ...
+    for link in soup.find_all('a'):
+        logger.debug(link.get('href'))
 
 
 if __name__ == "__main__":
