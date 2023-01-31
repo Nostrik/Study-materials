e515d4af567d1066ec9487984a50bcab5189f999
Nostrik <sairus.89@mail.ru>
2023-01-31 18:25:46 +0300

    task3_mod25

---
+++ module_25_asynchronous_programming/homework/hw_3/hw_3_internet_crawler.py
@@ -0,0 +1,13 @@
+from loguru import logger
+import aiohttp
+import time
+
+
+def main():
+    ...
+
+
+if __name__ == "__main__":
+    logger.info(f"started crawler at {time.strftime('%X')}")
+    main()
+    logger.info(f"finished crawler at {time.strftime('%X')}")
