adc1cbd382a55a3ad2f3328d3026b673be92f6b1
Nostrik <sairus.89@mail.ru>
2023-02-19 16:38:51 +0300

    worked_gunicorn_with_docker

---
+++ module_27_postgres_migrations/hw/app/flask_app.py
@@ -0,0 +1,12 @@
+from flask import Flask, Response
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return Response("Test PASS"), 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
