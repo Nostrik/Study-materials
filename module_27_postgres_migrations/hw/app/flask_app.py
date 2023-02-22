ac454550912ad34dcc33f1188cd8c523dba2407a
Nostrik <sairus.89@mail.ru>
2023-02-22 22:51:03 +0300

    task_mod_27

--- module_27_postgres_migrations/hw/app/flask_app.py
+++ module_27_postgres_migrations/hw/app/flask_app.py
@@ -1,6 +1,14 @@
 from flask import Flask, Response
+from sqlalchemy import Column, Integer, String, Float, \
+    create_engine, Sequence, Identity, ForeignKey, delete
+from sqlalchemy.orm import sessionmaker, declarative_base, relationship
+
 
 app = Flask(__name__)
+engine = create_engine('postgresql+psycopg2://admin:admin@localhost')
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+session = Session()
 
 
 @app.route("/")
