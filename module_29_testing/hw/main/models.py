5350aef673837020d59130f037e84df489f17590
Nostrik <sairus.89@mail.ru>
2023-03-22 16:02:04 +0300

    hw mod29

--- module_29_testing/hw/main/models.py
+++ module_29_testing/hw/main/models.py
@@ -3,4 +3,39 @@ from typing import Dict, Any
 
 
 class Client(db.Model):
+    __tablename__ = 'client'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    surname = db.Column(db.String(50), nullable=False)
+    credit_card = db.Column(db.String(10))
+
+    def __repr__(self):
+        return f"Client {self.name}"
+
+    def to_json(self) -> Dict[str, Any]:
+        return {c.name: getattr(self, c.name) for c in
+                self.__table__.columns}
+
+
+class Parking(db.Model):
+    __tablename__ = 'parking'
+
+    id = db.Column(db.Integer, primary_key=True)
+    address = db.Column(db.String(100), nullable=False)
+    opened = db.Column(db.Boolean)
+    count_places = db.Column(db.Integer, nullable=False)
+    count_available_places = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"Parking {self.id}, address {self.address}"
+
+    def to_json(self) -> Dict[str, Any]:
+        return {c.name: getattr(self, c.name) for c in
+                self.__table__.columns}
+
+
+class ClientParking(db.Model):
+    __tablename__ = 'client_parking'
+
     ...
